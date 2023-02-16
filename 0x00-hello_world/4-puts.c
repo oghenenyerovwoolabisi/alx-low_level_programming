f789446da49e7da01f5c02124e5f9c14f979b6fd
evelyn <e.okeremeta@gmail.com>
2023-02-16 14:36:24 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using put instead of printf to print
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
