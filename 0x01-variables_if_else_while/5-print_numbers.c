ff9c14eefdf457b13d009c9e22747767944028f1
evelyn <e.okeremeta@gmail.com>
2023-02-19 21:11:15 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints single digit numbers in base 10
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+		printf('\n');
+		return(0);
+}
