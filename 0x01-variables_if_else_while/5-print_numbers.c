4100b5e3c6f55e5ae59aa9066f6a0939f6e9efec
evelyn <e.okeremeta@gmail.com>
2023-02-19 21:15:55 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	int n;
+	int num;
 
-	for (n = '0'; n < 10; n++)
+	for (num = '0'; num < 10; num++)
 	{
-		printf("%d", n);
+		printf("%d", num);
 	}
 		printf('\n');
 		return(0);
