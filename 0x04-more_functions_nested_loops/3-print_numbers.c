8a8ea634dbf5c1e1fcac4458dbf14bf5b266c5d0
evelyn <e.okeremeta@gmail.com>
2023-02-23 18:44:13 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -30,11 +30,11 @@ int main(void)
 
 void print_numbers(void)
 {
-	int x;
+	char c;
 
-	for (x = '0'; x <= '9'; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(x + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
