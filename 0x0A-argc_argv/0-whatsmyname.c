fb1f2b7f27ce0e591c3c3b57e4df106aa3a818b9
evelyn <e.okeremeta@gmail.com>
2023-03-10 16:51:31 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a function that prints its name.
+ * @argc: number of things entered in the command line.
+ * @argv: array that holds string value of things
+ * entered in command line.
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+		i++;
+	}
+	return (0);
+}
