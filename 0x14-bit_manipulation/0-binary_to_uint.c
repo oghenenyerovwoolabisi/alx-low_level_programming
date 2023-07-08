7a9f8a9c62e0a92004fac73fa0a0c3f85b4139d2
evelyn <e.okeremeta@gmail.com>
2023-07-07 17:24:00 -0700

    bit manipulation files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
+ * binary_to_uint - function converts a binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
